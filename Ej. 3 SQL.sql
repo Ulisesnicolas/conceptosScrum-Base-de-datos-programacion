6711b2d9e239ecfb3cb108275429493ce88081cf
AbrilDuran20 <abrilduran20@gmail.com>
2022-08-13 18:43:11 -0300

    Ejercicio 3 SQL

--- Ej. 3 SQL.sql
+++ Ej. 3 SQL.sql
@@ -1,7 +1,8 @@
 
 delete from Perro where id_perro in (
-select id_perro from Historial where EXTRACT(YEAR FROM fecha) < 2010
+select distinct perro from Historial where EXTRACT(YEAR FROM fecha) < 2010
 )
+
 
 
 
